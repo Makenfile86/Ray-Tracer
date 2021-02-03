2d8224685fff73ec7418feb61acf5ee00dbc44fb
Makenfile86 <57939962+Makenfile86@users.noreply.github.com>
2021-02-03 10:00:59 +0200

    Add files via upload

--- libft/ft_free_strarray.c
+++ libft/ft_free_strarray.c
@@ -1,31 +1,22 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_strcpy.c                                        :+:      :+:    :+:   */
+/*   ft_strlen.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: mkivipur <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2019/08/19 14:41:13 by mkivipur          #+#    #+#             */
-/*   Updated: 2019/10/30 07:44:39 by mkivipur         ###   ########.fr       */
+/*   Created: 2019/10/15 12:24:15 by mkivipur          #+#    #+#             */
+/*   Updated: 2019/10/31 11:37:44 by mkivipur         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char	*ft_strcpy(char *dst, const char *src)
-{
-	size_t i;
-
-	i = 0;
-	if (src && dst)
-	{
-	while (src[i] != '\0')
-	{
-		dst[i] = src[i];
-		i++;
-	}
-	dst[i] = '\0';
-	return (dst);
-	}
-	return (NULL);
+void	ft_free_strarray(char **array)
+{	size_t i;
+	
+    i = 0;	
+    while (array[i])		
+    free(array[i++]);	
+    free(array);
 }
